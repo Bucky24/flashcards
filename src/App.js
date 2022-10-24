9292701c5e6b101d124d371cfd0df93631c26fef
Bucky24 <rwijtman@gmail.com>
2022-10-24 16:09:30 -0700

    new data and bug fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import classNames from 'classnames';
 import styles from './styles.css';
 
 import dutchData from './data/dutch.json';
+import GearImg from '../assets/gear.png';
 
 const allCategories = Object.keys(dutchData);
 const allAnswers = allCategories.reduce((answers, category) => {
@@ -19,8 +20,6 @@ const allAnswers = allCategories.reduce((answers, category) => {
 	];
 }, []);
 
-console.log(allAnswers);
-
 export default function App() {
 	const [cards, setCards] = useState([]);
 	const [activeCard, setActiveCard] = useState(0);
@@ -29,133 +28,165 @@ export default function App() {
 	const [correct, setCorrect] = useState(0);
 	const [wrong, setWrong] = useState(0);
 	const [categories, setCategories] = useState(allCategories);
+	const [showSettings, setShowSettings] = useState(false);
 
 	function reset() {
 		setWrong(0);
 		setCorrect(0);
 		setActiveCard(0);
 
-		const newCards = [];
-
-		for (const category of allCategories) {
+		const allCards = {};
+		for (const category of categories) {
 			const cardData = dutchData[category];
-			const allCards = Object.keys(cardData);
-
-			while (allCards.length > 0) {
-				// find a random questnio
-				const randomCardIndex = Math.floor(Math.random() * allCards.length);
-				const cardKey = allCards[randomCardIndex];
-				allCards.splice(randomCardIndex, 1);
-				const correctAnswer = cardData[cardKey];
-
-				const answers = [
-					correctAnswer,
-				];
-
-				// build a list of 4 answers. 3 chosen at random
-				const newAnswers = [...allAnswers];
-				while (newAnswers.length > 0 && answers.length < 4) {
-					const randomAnswerIndex = Math.floor(Math.random() * newAnswers.length);
-					const answer = newAnswers[randomAnswerIndex];
-					newAnswers.splice(randomAnswerIndex, 1);
-					if (!answers.includes(answer)) {
-						answers.push(answer);
-					}
-				}
-
-				// now shuffle the answers we found
-				const shuffledAnswers = [];
-				while (answers.length > 0) {
-					const randomAnswerIndex = Math.floor(Math.random() * answers.length);
-					const answer = answers[randomAnswerIndex];
-					answers.splice(randomAnswerIndex, 1);
-					shuffledAnswers.push(answer);
-				}
-
-				newCards.push({
-					question: cardKey,
-					answers: shuffledAnswers,
-					correctAnswer,
-				});
+			for (const key in cardData) {
+				allCards[key] = cardData[key];
 			}
+		}
+
+		const allCardNames = Object.keys(allCards);
+		const newCards = [];
+		while (allCardNames.length > 0) {
+			// find a random questnio
+			const randomCardIndex = Math.floor(Math.random() * allCardNames.length);
+			const cardKey = allCardNames[randomCardIndex];
+			allCardNames.splice(randomCardIndex, 1);
+			const correctAnswer = allCards[cardKey];
+
+			const answers = [
+				correctAnswer,
+			];
+
+			// build a list of 4 answers. 3 chosen at random
+			const newAnswers = [...allAnswers];
+			while (newAnswers.length > 0 && answers.length < 4) {
+				const randomAnswerIndex = Math.floor(Math.random() * newAnswers.length);
+				const answer = newAnswers[randomAnswerIndex];
+				newAnswers.splice(randomAnswerIndex, 1);
+				if (!answers.includes(answer)) {
+					answers.push(answer);
+				}
+			}
+
+			// now shuffle the answers we found
+			const shuffledAnswers = [];
+			while (answers.length > 0) {
+				const randomAnswerIndex = Math.floor(Math.random() * answers.length);
+				const answer = answers[randomAnswerIndex];
+				answers.splice(randomAnswerIndex, 1);
+				shuffledAnswers.push(answer);
+			}
+
+			newCards.push({
+				question: cardKey,
+				answers: shuffledAnswers,
+				correctAnswer,
+			});
 		}
 
 		setCards(newCards);
 	}
 
-	useEffect(reset, []);
+	useEffect(() => {
+		reset();
+	}, []);
 
 	const done = activeCard >= cards.length;
 	const currentCard = done ? null : cards[activeCard];
 
 	return (<div className={styles.appRoot}>
-		<header className={styles.header}>
-			<div className={styles.headerInner}>
-				<span>Correct: {correct}/{cards.length}</span>
-				<span>Wrong: {wrong}/{cards.length}</span>
-			</div>
-		</header>
-		<div className={styles.message}>{message}</div>
-		{!done && <section className={styles.cardHolder}>
-			<div className={styles.card}>
-				<h2>
-					{currentCard.question}
-				</h2>
-				<div className={styles.buttonHolder}>
-					{currentCard.answers.map((answer, index) => {
-						const correctAnswer = currentCard.correctAnswer === answer;
-						const guessed = guess === answer;
-						return (
-							<button
-								key={`answer_${index}`}
-								className={classNames(
-									styles.button,
-									correctAnswer && guessed && styles.correct,
-									!correctAnswer && guessed && styles.wrong,
-								)}
-								onClick={() => {
-									if (guess !== null) {
-										return;
-									}
-									if (correctAnswer) {
-										setMessage("Yes! The correct answer is \"" + currentCard.correctAnswer + "\"!");
-										setCorrect((correct) => {
-											return correct + 1;
-										});
-									} else {
-										setMessage("Incorrect! The correct answer is \"" + currentCard.correctAnswer + "\"");
-										setWrong((wrong) => {
-											return wrong + 1;
-										});
-									}
-									setGuess(answer);
-									setTimeout(() => {
-										setMessage("");
-										setActiveCard((activeCard) => {
-											return activeCard + 1;
-										});
-										setGuess(null);
-									}, correctAnswer? 500 : 1000);
-								}}
-							>
-								{answer}
-							</button>
-						);
-					})}
+		{!showSettings && <>
+			<header className={styles.header}>
+				<div className={styles.headerInner}>
+					<span>Correct: {correct}/{cards.length}</span>
+					<span>Wrong: {wrong}/{cards.length}</span>
 				</div>
-			</div>
-		</section>}
-		{done && <section style={{
-			display: 'flex',
-			flexDirection: 'column',
-			alignItems: 'center',
-		}}>
-			All Done!<br/>
-			<button
-				onClick={reset}
+			</header>
+			<div className={styles.message}>{message}</div>
+			{!done && <section className={styles.cardHolder}>
+				<div className={styles.card}>
+					<h2>
+						{currentCard.question}
+					</h2>
+					<div className={styles.buttonHolder}>
+						{currentCard.answers.map((answer, index) => {
+							const correctAnswer = currentCard.correctAnswer === answer;
+							const guessed = guess === answer;
+							return (
+								<button
+									key={`answer_${index}`}
+									className={classNames(
+										styles.button,
+										correctAnswer && guessed && styles.correct,
+										!correctAnswer && guessed && styles.wrong,
+									)}
+									onClick={() => {
+										if (guess !== null) {
+											return;
+										}
+										if (correctAnswer) {
+											setMessage("Yes! The correct answer is \"" + currentCard.correctAnswer + "\"!");
+											setCorrect((correct) => {
+												return correct + 1;
+											});
+										} else {
+											setMessage("Incorrect! The correct answer is \"" + currentCard.correctAnswer + "\"");
+											setWrong((wrong) => {
+												return wrong + 1;
+											});
+										}
+										setGuess(answer);
+										setTimeout(() => {
+											setMessage("");
+											setActiveCard((activeCard) => {
+												return activeCard + 1;
+											});
+											setGuess(null);
+										}, correctAnswer? 500 : 1000);
+									}}
+								>
+									{answer}
+								</button>
+							);
+						})}
+					</div>
+				</div>
+			</section>}
+			{done && <section style={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+			}}>
+				All Done!<br/>
+				<button
+					onClick={reset}
+				>
+					Go Again
+				</button>
+			</section>}
+			<div
+				style={{
+					position: 'fixed',
+					bottom: 0,
+					right: 0,
+					padding: 10,
+					cursor: 'pointer',
+				}}
+				onClick={() => {
+					setShowSettings(true);
+				}}
 			>
-				Go Again
-			</button>
-		</section>}
+				<img
+					src={GearImg}
+					style={{
+						width: 64,
+					}}
+				/>
+			</div>
+		</>}
+		{showSettings && <>
+			<section className={styles.cardHolder}>
+				foo
+			</section>
+		</>}
 	</div>);
 }
