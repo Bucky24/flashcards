b1d53bdebbf50b5d1bf5f1144375d54100945ee2
Bucky24 <rwijtman@gmail.com>
2022-12-02 15:25:27 -0800

    Flashcard image to go back from settings

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -2,11 +2,33 @@ import React from 'react';
 
 import styles from '../styles.css';
 
+import Flashcards from '../../assets/flashcards.png';
+
 export default function Settings({ setShowSettings }) {
     return (
-        <div className={styles.cardHolder}>
-            Settings
-            <button onClick={() => { setShowSettings(false) }}>Back</button>
-        </div>
+        <>
+            <div className={styles.cardHolder}>
+                Settings
+            </div>
+            <div
+				style={{
+					position: 'fixed',
+					bottom: 0,
+					right: 0,
+					padding: 10,
+					cursor: 'pointer',
+				}}
+				onClick={() => {
+					setShowSettings(false);
+				}}
+			>
+				<img
+					src={Flashcards}
+					style={{
+						width: 64,
+					}}
+				/>
+			</div>
+        </>
     );
 }
